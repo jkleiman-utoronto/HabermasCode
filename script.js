4cc6a850a59a470d7c5a8bc0a934787c219be895
Matt Price <matt.price@utoronto.ca>
2019-09-17 13:24:49 -0400

    remove hideous code. add an onlad function w/ a timeout.

--- script.js
+++ script.js
@@ -1,39 +1,22 @@
-if (document.getElementById('hello')) {
-  document.getElementById('hello').innerHTML = 'Hey where did this text come from? can you find it in the code? Can you change it?';
+function setup () {
+  let h = document.querySelector('#hello'); 
+  if (h) {
+    h.innerHTML = 'Hey where did this text come from? can you find it in the code? Can you change it?';
+    h.classList.add('bugout');
+  }
+  document.getElementsByClassName('button')[0].onclick = function() {
+    if(this.innerHTML === 'Click Me') 
+    { 
+      this.innerHTML = 'Make it Stop!';
+      this.classList.toggle('do-transition');
+    } else {
+      this.innerHTML = 'Click Me';
+      this.classList.toggle('do-transition');
+    }  
+  };
 }
 
-var crazyEl = document.getElementById('bad-news');
 
-// document.getElementsByClassName('button')[0].onclick = function() {
-//   if(this.innerHTML === 'Click Me') 
-//   { 
-//     this.innerHTML = 'Pause';
-//     crazyEl.classList.add('horizTranslate');
-//   } else {
-//     this.innerHTML = 'Click Me';
-//     var computedStyle = window.getComputedStyle(crazyEl),
-//         marginLeft = computedStyle.getPropertyValue('margin-left');
-//     crazyEl.style.marginLeft = marginLeft;
-//     crazyEl.classList.remove('horizTranslate');    
-//   }  
-// }
+window.onload = setTimeout(setup, 3000);
 
-document.getElementsByClassName('button')[0].onclick = function() {
-  if(this.innerHTML === 'Click Me') 
-  { 
-    this.innerHTML = 'Make it Stop!';
-    crazyEl.classList.add('do-transition');
-  } else {
-    this.innerHTML = 'Click Me';
-    var computedStyle = window.getComputedStyle(crazyEl),
-        marginLeft = computedStyle.getPropertyValue('margin-left'),
-        fontSize = computedStyle.getPropertyValue('font-size'),
-        background = computedStyle.getPropertyValue('background-color'),
-        border = computedStyle.getPropertyValue('border-color');
-    // crazyEl.style.marginLeft = marginLeft;
-    // crazyEl.style.fontSize = fontSize;
-    // crazyEl.style.borderColor = border;
-    // crazyEl.style.backgroundColor = background;
-    crazyEl.classList.remove('do-transition');    
-  }  
-}
+
